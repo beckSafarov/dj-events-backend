68f5b81a0cc2cd08990217d990c6f73aaaf69e69
Begzod Safarov <begzod.safarov07@gmail.com>
2023-03-26 14:02:59 +0500

    event.me controller

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,9 +1,24 @@
 'use strict';
-
 /**
  * event controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({ strapi }) => ({
+  async me(context) {
+    const user = context.state.user
+    if (!user) {
+      return context.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] }
+      ])
+    }
+    const data = await strapi.db.query("api::event.event").findMany({
+      where: {user: {id: user.id}},
+      populate: {user: true, image: true}
+    })
+    if (!data) return context.notFound()
+
+    return await this.sanitizeOutput(data, context)
+  }
+}));
